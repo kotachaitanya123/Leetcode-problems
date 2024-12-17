f53d8a49a0710d4bbd187f7c09a1bef0929f7546
kotachaitanya123 <94459283+kotachaitanya123@users.noreply.github.com>
2024-12-17 20:31:09 +0530

    Update 21. Merge Two Sorted Lists.java

--- Linked List/Easy/21. Merge Two Sorted Lists.java
+++ Linked List/Easy/21. Merge Two Sorted Lists.java
@@ -2,8 +2,8 @@ TC - O(N)
 SC - O(1)
 // The below code is merging two sorted lists and the answer should also be a sorted linked list as the two lists are in sorted order we are expected to solve the
 // problem in O(N) and O(1) in interview point of view and we can solve this using two-pointers technique, where one pointer will be at the starting point of the list1
-// and second pointer will be at the start of the second list and we will use a while loop and keep track of the both pointers by moving and adding them to the empty
-// list based on the value we come across in the lists.
+// and second pointer will be at the start of the list2 and we will use a while loop and keep track of the both pointers by moving and adding them to the empty
+// list based on the value we come across in both the lists.
 
 /**
  * Definition for singly-linked list.
@@ -17,9 +17,12 @@ SC - O(1)
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // initialize an empty list
         ListNode ans = new ListNode();
         ListNode cur = ans;
-
+        // iterate until one of the list of the two lists is empty and in each iteration add the value which is less than the other in whatsoever list to the 
+        // ans, and move that particular list to the next step and cur to next pointer, by the time one of the list completes you will have all the values of that list
+        // in the ans.
         while(list1 != null && list2 != null) {
             if(list1.val < list2.val) {
                 cur.next = list1;
@@ -32,7 +35,10 @@ class Solution {
             }
         }
 
+        // After completing one of the list we dont know which list gets completed by the end of the while so we will check that using ternary operator on the two 
+        // lists and add the remaining list to the ans.
         cur.next = (list1 != null) ? list1 : list2;
+        // return ans.next
         return ans.next;
          
     }
