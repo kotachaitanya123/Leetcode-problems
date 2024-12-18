fb8866d80884ce58928d2254e06e5fd9e744fb51
kotachaitanya123 <94459283+kotachaitanya123@users.noreply.github.com>
2024-12-18 21:33:30 +0530

    Update 21. Merge Two Sorted Lists.java

--- Linked List/Easy/21. Merge Two Sorted Lists.java
+++ Linked List/Easy/21. Merge Two Sorted Lists.java
@@ -1,9 +1,10 @@
 TC - O(N)
 SC - O(1)
-// The below code is merging two sorted lists and the answer should also be a sorted linked list as the two lists are in sorted order we are expected to solve the
-// problem in O(N) and O(1) in interview point of view and we can solve this using two-pointers technique, where one pointer will be at the starting point of the list1
-// and second pointer will be at the start of the list2 and we will use a while loop and keep track of the both pointers by moving and adding them to the empty
-// list based on the value we come across in both the lists.
+    
+The below code is merging two sorted lists and the answer should also be a sorted linked list as the two lists are in sorted order we are expected to solve the
+problem in O(N) and O(1) in interview point of view and we can solve this using two-pointers technique, where one pointer will be at the starting point of the list1
+and second pointer will be at the start of the list2 and we will use a while loop and keep track of the both pointers by moving and adding them to the empty
+list based on the value we come across in both the lists.
 
 /**
  * Definition for singly-linked list.
