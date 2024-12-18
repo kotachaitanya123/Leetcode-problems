25a2a44836a1eb297c45993ef1f883e656d7379d
kotachaitanya123 <94459283+kotachaitanya123@users.noreply.github.com>
2024-12-18 21:33:43 +0530

    Update 83. Remove Duplicates from Sorted List.java

--- Linked List/Easy/83. Remove Duplicates from Sorted List.java
+++ Linked List/Easy/83. Remove Duplicates from Sorted List.java
@@ -1,9 +1,9 @@
 TC - O(N)
 SC - O(1)
 
-// The below code is used to remove the duplicates from the sorted list, as it is sorted we will assign the cur to head, while we are moving from left to the right
-// and at each point we will see whether the next value of the cur value is equal to it whenever we find like that we will simply move out cur pointer to the cur.next.next
-// and return head at the last.
+The below code is used to remove the duplicates from the sorted list, as it is sorted we will assign the cur to head, while we are moving from left to the right
+and at each point we will see whether the next value of the cur value is equal to it whenever we find like that we will simply move out cur pointer to the cur.next.next
+and return head at the last.
 
 /**
  * Definition for singly-linked list.
