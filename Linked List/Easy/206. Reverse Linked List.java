f468b26c6d20952f6bb385eef3e2cd3d69e4aefa
kotachaitanya123 <94459283+kotachaitanya123@users.noreply.github.com>
2024-12-18 21:33:14 +0530

    Update 206. Reverse Linked List.java

--- Linked List/Easy/206. Reverse Linked List.java
+++ Linked List/Easy/206. Reverse Linked List.java
@@ -1,11 +1,11 @@
 TC - O(N)
 SC - O(1)
 
-// The reversing of the linked list is the reversing the numbers in the list its about the pointing of the whole list in a reverse order.
-// We need two pointers one is prev and the other is cur which start from the starting point of the head, we will move towards the end of the list while moving until
-// the cur != null we will have another pointer(cur_) that points to the cur.next in every iteration that is next node of the list and here starts our logic like we 
-// will point cur.next to prev where will remove the link between cur.next and point it towards the prev and make the prev node as cur node and we will assign our cur
-// cur_ which at the cur.next in every iteration, by the time the cur == null the loop will break and we will return the prev.
+The reversing of the linked list is the reversing the numbers in the list its about the pointing of the whole list in a reverse order.
+We need two pointers one is prev and the other is cur which start from the starting point of the head, we will move towards the end of the list while moving until
+the cur != null we will have another pointer(cur_) that points to the cur.next in every iteration that is next node of the list and here starts our logic like we 
+will point cur.next to prev where will remove the link between cur.next and point it towards the prev and make the prev node as cur node and we will assign our cur
+cur_ which at the cur.next in every iteration, by the time the cur == null the loop will break and we will return the prev.
 
 /**
  * Definition for singly-linked list.
